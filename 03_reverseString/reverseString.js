3138a00b15ed8382fd36da9e2c1d238ffb257040
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2023-11-01 12:22:44 +0530

    Solve reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(userString) {
+    let revString = "";
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+    }
+    return revString;
 };
 
 // Do not edit below this line
