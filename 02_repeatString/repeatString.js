43cb8113e96e1c8858719acf236c1c1704eb0cc5
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2023-11-01 12:15:51 +0530

    Solve repeatString exercise

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,16 @@
-const repeatString = function() {
+const repeatString = function(stringUser, num) {
+    let result = "";
 
+    if (num < 0) {
+        return "ERROR";
+    } else if (stringUser === "") {
+        return "";
+    }
+
+    for (let i = 0; i < num; i++) {
+        result += stringUser;
+    }
+    return result;
 };
 
 // Do not edit below this line
