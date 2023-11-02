a682a346f52cac24c8347a353d251b56cc4a67dd
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2023-11-02 13:21:54 +0530

    solve sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let num, sum = 0;
 
+    if (typeof start === 'string' || typeof end === 'string') {
+        return "ERROR";
+    } else if (isNaN(start) || isNaN(end)) {
+        return "ERROR";
+    } else if (start < 0 || end < 0) {
+        return "ERROR";
+    } else {
+        if (start < end) {
+            for (num = start; num <= end; num++) {
+                sum += num;
+            }
+        } else {
+            for (num = end; num <= start; num++) {
+                sum += num;
+            }
+        }
+        return sum;  
+    }
+    
 };
 
 // Do not edit below this line
