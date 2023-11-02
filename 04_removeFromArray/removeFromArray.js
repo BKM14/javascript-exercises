adc0637b3f08c1f48714410fcc22ba7e1ba4ec71
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2023-11-02 12:52:03 +0530

    Solve removeFromArray exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(userArray) {
+    let index;
+    let elementsToRemove = [];
+    let finalArray = []
+
+    for (index = 1; index < arguments.length; index++) {
+        elementsToRemove.push(arguments[index]);
+    }
+    
+    for (const element of userArray) {
+        if (!(elementsToRemove.includes(element))) {
+            finalArray.push(element);
+        }
+    }
+
+    return finalArray;
 
 };
 
