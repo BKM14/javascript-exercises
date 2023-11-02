18c8d33a2de336cd3480e43aacffb11fbea24110
Balaji Krishnamurthy <balajikrishnamurthy2004@gmail.com>
2023-11-02 14:36:20 +0530

    solve leapYears exercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,17 @@
-const leapYears = function() {
-
+const leapYears = function(yearInput) {
+    if (yearInput % 4 == 0) {
+        if (yearInput % 100 == 0) {
+            if (yearInput % 400 == 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return true;
+        }
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
